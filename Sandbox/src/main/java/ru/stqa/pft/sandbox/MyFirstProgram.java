119b5dc3e82b088e3b52b8becd7e7663f2a35b53
infernalcfyz <InfernalCfyz@mail.ru>
2021-06-06 22:47:14 +0700

    Считаем расстояние между точками без метода

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,6 +5,23 @@ public class MyFirstProgram {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello, world!");
+		Square s = new Square(5);
+		Rectangle r = new Rectangle(4, 6);
+		Point start = new Point();
+		Point end = new Point ();
+		start.x = 1;
+		start.y = 1;
+		end.x = 1;
+		end.y = 2;
+		double d = distance(start, end);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+		System.out.println("Расстояние между точками = " + d);
 	}
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+	}
+
+
 
 }
