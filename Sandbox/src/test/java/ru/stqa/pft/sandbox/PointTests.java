f9abb1327029d9e50631b89c0fffe335d10a04f4
infernalcfyz <InfernalCfyz@mail.ru>
2021-06-08 22:50:57 +0700

    Добавлено ещё несколько тестов

--- Sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ Sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -11,5 +11,20 @@ public class PointTests {
         Assert.assertEquals(start.distance(end) , 1);
 
     }
+    @Test
+    public void testDistance1 () {
+        Point start = new Point (1,4);
+        Point end = new Point(1,3);
+        Assert.assertEquals(start.distance(end) , 1);
 
+    }
+    @Test
+    public void testDistance2 () {
+        Point start = new Point (1,3);
+        Point end = new Point(1,-1);
+        Assert.assertEquals(start.distance(end) , 4);
+
+    }
 }
+
+
