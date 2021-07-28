2596d8b5eda1490cd5c0b029a6cb21bd59054509
infernalcfyz <InfernalCfyz@mail.ru>
2021-07-29 02:30:04 +0700

    Добавил удоление групп

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,13 +2,13 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import ru.stqa.pft.addressbook.model.ContactData;
 
-public class GroupHelper {
-    private WebDriver wd;
+public class GroupHelper extends HelperBase {
     public GroupHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
+
     }
+    public void returnToGroupPage () {wd.findElement(By.linkText("group page")).click();}
 
     public void submitGroupCreation() {
       wd.findElement(By.name("submit")).click();
@@ -30,31 +30,9 @@ public class GroupHelper {
       wd.findElement(By.name("new")).click();
     }
 
-    public void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-        wd.findElement(By.name("middlename")).clear();
-        wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-        wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys(contactData.getNick());
-        wd.findElement(By.name("title")).clear();
-        wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
-        wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys(contactData.getJob());
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-        wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys(contactData.getHome());
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-        wd.findElement(By.name("work")).clear();
-        wd.findElement(By.name("work")).sendKeys(contactData.getWork());
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-        wd.findElement(By.xpath("//div[@id='content']/div")).click();
-    }
+    public void deleteSelectedGroups() {wd.findElement(By.name("delete")).click();}
+
+    public void selectGroup () {wd.findElement(By.name("selected[]")).click();}
+
+
 }
