bd43bd42f06667fcc693c3828111c0d80cb7d366
infernalcfyz <InfernalCfyz@mail.ru>
2021-08-03 23:52:56 +0700

    Реализована проверка и обеспечение выполнение предусловий

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -30,11 +30,23 @@ public class GroupHelper extends HelperBase {
     public void selectGroup () {click(By.name("selected[]"));}
 
 
+
     public void initGroupModification() {
         click (By.name("edit"));
     }
 
     public void submitGroupModification() {
         click (By.name("update"));
+    }
+
+    public void createGroup(GroupData group) {
+        initGroupCreation();
+        fillGroupForm (group);
+        submitGroupCreation();
+        returnToGroupPage();
+    }
+
+    public boolean isThereAGroup() {
+        return isElementPresent(By.name("selected[]"));
     }
 }
--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -8,6 +8,9 @@ public class GroupModificationTests extends TestBase{
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup (new GroupData("11", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm (new GroupData("11", "44", "55"));
