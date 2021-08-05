9b158db2dd164a694da15743c9519fd33a80c3a4
infernalcfyz <InfernalCfyz@mail.ru>
2021-08-05 10:17:24 +0700

    Проверка при модификации и добавлении контакта

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEditTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEditTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -8,6 +9,8 @@ public class ContactEditTests extends TestBase{
     @Test
 
     public void testContactEdit () {
+        app.getNavigationHelper().returnToHomePage();
+        int before = app.getContactHelper().getContactCount();
         if (! app.getContactHelper().isThereAGroup()) {
             app.getContactHelper().createContact (new ContactData("11", null, "33", "44", "55", "66",
                     "77", "88", "99", "1010", "1234", "11"), true);
@@ -17,6 +20,8 @@ public class ContactEditTests extends TestBase{
         app.getContactHelper().fillContactForm (new ContactData("11", "Olegovich", "Markin", "RaccoonCfyz", "qwe", "rty", "123", "456", "0987", "hasbeendone", "killmepls", null), false);
         app.getContactHelper().submitContactEdit();
         app.getNavigationHelper().returnToHomePage();
+        int after = app.getContactHelper().getContactCount();
+        Assert.assertEquals(after,before);
         app.getNavigationHelper().Logout();
     }
 }
--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/TestContact.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/TestContact.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -7,10 +8,14 @@ public class TestContact extends TestBase {
 
   @Test
   public void testContact() {
+    app.getNavigationHelper().returnToHomePage();
+    int before = app.getContactHelper().getContactCount();
     app.getNavigationHelper().gotoNewContact();
     app.getContactHelper().fillContactForm (new ContactData("11", null, "33", "44", "55", "66", "77", "88", "99", "1010", "1234", "11"), true);
     app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().returnToHomePage();
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after,before + 1);
     app.getNavigationHelper().Logout();
   }
 
