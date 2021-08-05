148b3a8923683100a5371c2e6bf4ae33968f3e32
infernalcfyz <InfernalCfyz@mail.ru>
2021-08-05 10:15:17 +0700

    Удаление и модификация не только первой группы по списку, проверка при удалении контакта

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -8,6 +9,8 @@ public class ContactDeletionTests extends TestBase{
     @Test
 
     public void testContactDeletion() {
+        app.getNavigationHelper().returnToHomePage();
+        int before = app.getContactHelper().getContactCount();
         if (! app.getContactHelper().isThereAGroup()) {
             app.getContactHelper().createContact (new ContactData("11", null, "33", "44", "55", "66",
                     "77", "88", "99", "1010", "1234", "11"), true);
@@ -15,5 +18,9 @@ public class ContactDeletionTests extends TestBase{
         }
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteContact();
+        app.getNavigationHelper().returnToHomePage();
+        int after = app.getContactHelper().getContactCount();
+        Assert.assertEquals(after,before - 1);
+        app.getNavigationHelper().Logout();
     }
 }
--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase{
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup (new GroupData("11", null, null));
         }
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm (new GroupData("11", "44", "55"));
         app.getGroupHelper().submitGroupModification();
