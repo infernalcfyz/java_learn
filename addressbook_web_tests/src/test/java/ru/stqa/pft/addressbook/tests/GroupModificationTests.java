7cabc6a34859778fd2aa539c857a853ad1d81c2a
infernalcfyz <InfernalCfyz@mail.ru>
2021-08-05 09:36:05 +0700

    Контроль количества элементов при создании, удалении и модификации групп

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,6 +9,7 @@ public class GroupModificationTests extends TestBase{
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup (new GroupData("11", null, null));
         }
@@ -16,6 +18,8 @@ public class GroupModificationTests extends TestBase{
         app.getGroupHelper().fillGroupForm (new GroupData("11", "44", "55"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after,before);
         app.getNavigationHelper().Logout();
     }
 }
--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/TestGroup4.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/TestGroup4.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,10 +9,13 @@ public class TestGroup4 extends TestBase {
   @Test
   public void testGroup4() {
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm (new GroupData("11", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after,before + 1);
     app.getNavigationHelper().Logout();
   }
 
