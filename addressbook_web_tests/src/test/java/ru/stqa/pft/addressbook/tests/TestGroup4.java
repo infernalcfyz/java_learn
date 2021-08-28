87953074d6a32d9e441637edbf3af7f293ea3687
infernalcfyz <InfernalCfyz@mail.ru>
2021-08-28 12:12:48 +0700

    Оптимизирован код в тестах на создание, модификацию и удаление групп

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/TestGroup4.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/TestGroup4.java
@@ -15,13 +15,9 @@ public class TestGroup4 extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
     GroupData group = new GroupData("11", null, null);
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm (group);
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size() + 1);
-
     group.setId(after.stream().max( (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
